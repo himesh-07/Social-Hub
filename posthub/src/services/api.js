ad5831f23d0ced325c68d762d488a6538211ed74
Himesh Thakur <thakurhimesh546@gmail.com>
2026-06-03 19:50:29 +0530

    coma

--- posthub/src/services/api.js
+++ posthub/src/services/api.js
@@ -1,6 +1,4 @@
-import axios from "axios";
-
 const API = axios.create({
   console.log("API URL =", process.env.REACT_APP_API_URL);
+  baseURL: process.env.REACT_APP_API_URL,
 });
-export default API;
