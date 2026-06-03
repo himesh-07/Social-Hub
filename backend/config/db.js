e6e622a744c437a47d94fadb07b95d11aac7743e
Himesh Thakur <thakurhimesh546@gmail.com>
2026-06-03 19:46:28 +0530

    debug

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.REACT_APP_API_URL);
     console.log("MongoDB Connected");
   } catch (error) {
     console.log(error);
