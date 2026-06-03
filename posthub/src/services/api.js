8fc1e1d55ecebd7bc2ebe996c5c88303de5ca49d
Himesh Thakur <thakurhimesh546@gmail.com>
2026-06-03 17:35:43 +0530

    Update api.js

--- posthub/src/services/api.js
+++ posthub/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://social-hub-5fk0.onrender.com/api",
+  console.log("API URL =", process.env.REACT_APP_API_URL);
 });
 export default API;
