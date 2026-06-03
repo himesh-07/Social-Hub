417b30e4ac72f6471f2aa476203e9e6acb858f37
Himesh Thakur <thakurhimesh546@gmail.com>
2026-06-03 17:00:11 +0530

    Update API base URL to production endpoint

--- posthub/src/services/api.js
+++ posthub/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL:  process.env.REACT_APP_API_URL + "/api",
+  baseURL: "https://social-hub-5fk0.onrender.com/api",
 });
 export default API;
